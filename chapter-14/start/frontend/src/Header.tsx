9db322a04b887a95dfac26f0a22ca6ea3ed38797
aurzin <aurzin@gmail.com>
2021-05-18 23:44:14 +0300

    Add !

--- chapter-14/start/frontend/src/Header.tsx
+++ chapter-14/start/frontend/src/Header.tsx
@@ -69,7 +69,7 @@ export const Header = () => {
           text-decoration: none;
         `}
       >
-        Q & A
+        Q & A!
         <span
           css={css`
             margin-left: 5px;
