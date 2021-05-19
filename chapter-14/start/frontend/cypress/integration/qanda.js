16564eab810360b88dca4216bf44f09cf210a58f
aurzin <aurzin@gmail.com>
2021-05-19 22:00:01 +0300

    Fix cypress.json

--- chapter-14/start/frontend/cypress/integration/qanda.js
+++ chapter-14/start/frontend/cypress/integration/qanda.js
@@ -7,35 +7,30 @@ describe('Ask question', () => {
     cy.contains('Unanswered Questions');
     cy.contains('Sign In').click();
     cy.url().should('include', 'auth0');
-    cy.findByLabelText('Email')
-      .type('your-username')
-      .should('have.value', 'your-username');
+    cy.findByLabelText('Email address')
+      .type('aurzin@live.ru')
+      .should('have.value', 'aurzin@live.ru');
 
     cy.findByLabelText('Password')
-      .type('your-password')
-      .should('have.value', 'your-password');
+      .type('fuckYou!')
+      .should('have.value', 'fuckYou!');
 
-    cy.get('form').submit();
+    cy.get('form:first').submit();
 
     cy.contains('Unanswered Questions');
-      
+
     cy.contains('Ask a question').click();
     cy.contains('Ask a question');
 
     var title = 'title test';
     var content = 'Lots and lots and lots and lots and lots of content test';
-    cy.findByLabelText('Title')
-      .type(title)
-      .should('have.value', title);
-    cy.findByLabelText('Content')
-      .type(content)
-      .should('have.value', content);
+    cy.findByLabelText('Title').type(title).should('have.value', title);
+    cy.findByLabelText('Content').type(content).should('have.value', content);
 
     cy.contains('Submit Your Question').click();
     cy.contains('Your question was successfully submitted');
-      
+
     cy.contains('Sign Out').click();
     cy.contains('You successfully signed out!');
-
   });
 });
